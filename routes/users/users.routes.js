2297d8c607c8f4ab6009bd0c271a64935642d58a
rafasanzvi <rafasv41@gmail.com>
2022-07-26 18:21:07 +0200

    favourites added

--- routes/users/users.routes.js
+++ routes/users/users.routes.js
@@ -64,6 +64,7 @@ router.get("/getUserFavs", isAuthenticated, (req, res) => {
     User
         .findById(user_id)
         .select('favouriteShirts')
+        .populate('favouriteShirts')
         .then(response => res.json(response.favouriteShirts))
         .catch(err => res.status(500).json(err))
 
@@ -97,6 +98,7 @@ router.get("/:user_id", (req, res) => {
 
     User
         .findById(user_id)
+        .populate('favouriteShirts')
         .then(response => res.json(response))
         .catch(err => res.status(500).json(err))
 })
